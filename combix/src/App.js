3d038cf9e0ec8e41f2a6d8b5fb09651913e633b0
marzofran <fpmarzano@gmail.com>
2021-04-19 00:18:54 -0300

    Co-authored-by: SergioDlr <SergioDlr@users.noreply.github.com> clovergreen24 <clovergreen24@users.noreply.github.com>

--- combix/src/App.js
+++ combix/src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://pbs.twimg.com/profile_images/1374038263409864714/vN1bxpTr_400x400.jpg"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          putO el que gira
         </p>
         <a
           className="App-link"
