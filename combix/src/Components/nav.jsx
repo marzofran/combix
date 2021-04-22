cb59507ced24c4983973e32fd0eb87e564a0da45
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-04-22 16:28:18 -0300

    Create nav.jsx

---
+++ combix/src/Components/nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+
+const Nav = () => {
+  return (
+    <div>
+      <Row>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+        <Col>Navigation</Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Nav;
