685f82c53451d5c318646aa500562acac0ff5a6f
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-04-23 09:30:37 -0300

    Update nav.jsx

--- combix/src/Components/nav.jsx
+++ combix/src/Components/nav.jsx
@@ -15,29 +15,27 @@ const Nav = () => {
     <div>
       <Router>
         <Navbar expand='lg' variant='dark' style={{backgroundColor: '#135671'}}>
-          <Container>
-            <Navbar.Brand href='#'>
-              <img
-                src='https://iili.io/BdjqsS.png'
-                width='40%'
-                className='d-inline-block align-top'
-                alt='Combix logo'
-              />
-            </Navbar.Brand>
-            <Link className='navbar-brand nav-link ' to='/aboutus'>
-              Sobre nosotros
-            </Link>
-            <Link className='navbar-brand nav-link ' to='/contact'>
-              Contactanos
-            </Link>
-            <Link
-              className='navbar-brand nav-link btn'
-              style={{backgroundColor: '#0f172e'}}
-              to='/login'
-            >
-              Iniciar Sesion
-            </Link>
-          </Container>
+          <Navbar.Brand href='#'>
+            <img
+              src='https://iili.io/BdjqsS.png'
+              width='20%'
+              className='d-inline-block align-top'
+              alt='Combix logo'
+            />
+          </Navbar.Brand>
+          <Link className='navbar-brand nav-link ' to='/aboutus'>
+            Sobre nosotros
+          </Link>
+          <Link className='navbar-brand nav-link ' to='/contact'>
+            Contactanos
+          </Link>
+          <Link
+            className='navbar-brand nav-link btn'
+            style={{backgroundColor: '#0f172e'}}
+            to='/login'
+          >
+            Iniciar Sesion
+          </Link>
           <Switch>
             <Route path='/login'>
               <h1>iniciarSesion Component</h1>
