c8026e9b4a76a0694d9bf3479db8329b2c8c6c6e
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-04-23 09:17:49 -0300

    Update nav.jsx

--- combix/src/Components/nav.jsx
+++ combix/src/Components/nav.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 const Nav = () => {
   return (
     <div>
-      <Row>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-        <Col>Navigation</Col>
-      </Row>
+      <Router>
+        <Navbar expand='lg' variant='dark' style={{backgroundColor: '#135671'}}>
+          <Container>
+            <Navbar.Brand href='#'>
+              <img
+                src='https://iili.io/BdjqsS.png'
+                width='40%'
+                className='d-inline-block align-top'
+                alt='Combix logo'
+              />
+            </Navbar.Brand>
+            <Link className='navbar-brand nav-link ' to='/aboutus'>
+              Sobre nosotros
+            </Link>
+            <Link className='navbar-brand nav-link ' to='/contact'>
+              Contactanos
+            </Link>
+            <Link
+              className='navbar-brand nav-link btn'
+              style={{backgroundColor: '#0f172e'}}
+              to='/login'
+            >
+              Iniciar Sesion
+            </Link>
+          </Container>
+          <Switch>
+            <Route path='/login'>
+              <h1>iniciarSesion Component</h1>
+            </Route>
+            <Route path='/aboutus'>
+              <h1>aboutus Component</h1>
+            </Route>
+            <Route path='/contact'>
+              <h1>contact Component</h1>
+            </Route>
+          </Switch>
+        </Navbar>
+      </Router>
     </div>
   );
 };
