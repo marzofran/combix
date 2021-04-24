6b4fe2d8224b040de18178c84fe8e79b01b7d79f
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-04-24 18:53:49 -0300

    Fixing validation
    Fixing validation

--- combix/src/Components/register.jsx
+++ combix/src/Components/register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Form, Card, Row, Col} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-import userRegisterForm from '../userRegisterForm'
+import userRegisterForm from '../userRegisterForm';
 
 const Register = () => {
   const {handleChange, values, handleSubmit, errors} = userRegisterForm();
@@ -13,46 +13,98 @@ const Register = () => {
           <Card.Title>
             <h2>Registrate</h2>
           </Card.Title>
-          <Form onSubmit={handleSubmit}>
+          <Form id={'formularioPPal'} onSubmit={handleSubmit}>
             <Form.Group controlId='nombre'>
               <Form.Label>Nombre/s</Form.Label>
-              <Form.Control type='name' placeholder='Nombre/s' name='nombre' value={values.nombre} onChange={handleChange}/>
+              <Form.Control
+                required
+                type='name'
+                placeholder='Nombre/s'
+                name='nombre'
+                value={values.nombre}
+                onChange={handleChange}
+              />
               {errors.nombre && <p>{errors.nombre}</p>}
             </Form.Group>
             <Form.Group controlId='apellido'>
               <Form.Label>Apellido/s </Form.Label>
-              <Form.Control type='name' placeholder='Apellido/s' name='apellido' value={values.apellido} onChange={handleChange}/>
+              <Form.Control
+                required
+                type='name'
+                placeholder='Apellido/s'
+                name='apellido'
+                value={values.apellido}
+                onChange={handleChange}
+              />
               {errors.apellido && <p>{errors.apellido}</p>}
             </Form.Group>
             <Row>
               <Col>
                 <Form.Group controlId='dni'>
                   <Form.Label>Dni</Form.Label>
-                  <Form.Control type='text' placeholder='Dni' name='dni' value={values.dni} onChange={handleChange}/>
+                  <Form.Control
+                    required
+                    type='text'
+                    minLength='7'
+                    placeholder='Dni'
+                    name='dni'
+                    value={values.dni}
+                    onChange={handleChange}
+                  />
                   {errors.dni && <p>{errors.dni}</p>}
                 </Form.Group>
               </Col>
               <Col>
                 <Form.Group controlId='fechaNacimiento'>
                   <Form.Label>Fecha de nacimiento</Form.Label>
-                  <Form.Control type='date' placeholder='Fecha de nacimiento' name='fechaNacimiento' value={values.fechaNacimiento} onChange={handleChange}/>
+                  <Form.Control
+                    required
+                    type='date'
+                    placeholder='Fecha de nacimiento'
+                    name='fechaNacimiento'
+                    value={values.fechaNacimiento}
+                    onChange={handleChange}
+                  />
                   {errors.fechaNacimiento && <p>{errors.fechaNacimiento}</p>}
                 </Form.Group>
               </Col>
             </Row>
             <Form.Group controlId='mail'>
               <Form.Label>Email</Form.Label>
-              <Form.Control type='email' placeholder='Email' name='mail' value={values.mail} onChange={handleChange}/>
+              <Form.Control
+                required
+                type='email'
+                placeholder='Email'
+                name='mail'
+                value={values.mail}
+                onChange={handleChange}
+              />
               {errors.mail && <p>{errors.mail}</p>}
             </Form.Group>
             <Form.Group controlId='clave'>
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type='password' placeholder='**********' name='clave' value={values.clave} onChange={handleChange}/>
+              <Form.Control
+                required
+                minLength='6'
+                type='password'
+                placeholder='**********'
+                name='clave'
+                value={values.clave}
+                onChange={handleChange}
+              />
               {errors.clave && <p>{errors.clave}</p>}
             </Form.Group>
             <Form.Group controlId='repetirClave'>
               <Form.Label>Repetir contraseña</Form.Label>
-              <Form.Control type='password' placeholder='**********' name='repetirClave' value={values.repetirClave} onChange={handleChange}/>
+              <Form.Control
+                required
+                minLength='6'
+                type='password'
+                placeholder='**********'
+                name='repetirClave'
+                value={values.repetirClave}
+                onChange={handleChange}
+              />
               {errors.repetirClave && <p>{errors.repetirClave}</p>}
             </Form.Group>
             <Row>
--- combix/src/userRegisterForm.js
+++ combix/src/userRegisterForm.js
@@ -1,91 +1,63 @@
-import {useState, useEffect} from 'react'
-import Axios from 'axios'
+import {useState, useEffect} from 'react';
+import Axios from 'axios';
 
-const UserRegisterForm = callback => {
-    const [values, setValues] = useState({
-        nombre: '',
-        apellido: '',
-        dni: '',
-        fechaNacimiento: '',
-        mail: '',
-        clave: '',
-        repetirClave: ''
-    })
-    const [errors, setErrors] = useState({})
-    const [isSubmitting, setIsSubmitting] = useState(false)
+const UserRegisterForm = (callback) => {
+  const [values, setValues] = useState({
+    nombre: '',
+    apellido: '',
+    dni: '',
+    fechaNacimiento: '',
+    mail: '',
+    clave: '',
+    repetirClave: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const handleChange = e => {
-        const {name, value} = e.target
-        setValues({
-            ...values,
-            [name]: value
-        })
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setErrors(validateInfo(values));
+    setIsSubmitting(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      const newUser = {
+        nombre: values.nombre,
+        apellido: values.apellido,
+        dni: values.dni,
+        fechaNacimiento: values.fechaNacimiento,
+        mail: values.mail,
+        clave: values.clave,
+      };
+
+      Axios.post('/users', newUser).then((response) => {
+        console.log('El registro fue exitoso');
+        // login
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }
+  }, [errors]);
 
-    const handleSubmit = event => {
-        event.preventDefault()
-        
-        setErrors(validateInfo(values))
-        setIsSubmitting(true)
-      }
+  return {handleChange, values, handleSubmit, errors};
+};
 
-      useEffect(()=>{
-        if(Object.keys(errors).length === 0 && isSubmitting){
-            const newUser = {
-                nombre: values.nombre,
-                apellido: values.apellido,
-                dni: values.dni,
-                fechaNacimiento: values.fechaNacimiento,
-                mail: values.mail,
-                clave: values.clave
-            }
+function validateInfo(values) {
+  let errors = {};
+  if (values.clave !== values.repetirClave) {
+    errors.repetirClave = 'Las contraseñas no concuerdan';
+  }
 
-            Axios
-            .post('/users', newUser )
-            .then(response => {
-                console.log('El registro fue exitoso')
-                // login
-            })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        }},[errors])
-
-    return {handleChange, values, handleSubmit, errors}
+  return errors;
 }
 
-function validateInfo(values){
-    let errors = {}
-    if(!values.nombre.trim()){
-        errors.nombre = "Nombre requerido"
-    }
-    if(!values.apellido.trim()){
-        errors.apellido = "Apellido requerido"
-    }
-    if(!values.dni.trim()){
-        errors.dni = "dni requerido"
-    } else if (values.dni.length < 7){
-        errors.dni = "dni invalido"
-    }
-    if(!values.fechaNacimiento.trim()){
-        errors.fechaNacimiento = "Fecha de nacimiento requerida"
-    }
-    if(!values.mail.trim()){
-        errors.mail = "Email requerido"
-    } else if ( !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email) ){
-        errors.mail = "Email inválido"
-    }
-    if(!values.clave){
-        errors.clave = "Contraseña requerida"
-    } else if (values.clave.length < 6){
-        errors.clave = "6 caracteres mínimo"
-    }
-    if(!values.repetirClave){
-        errors.repetirClave = "Contraseña requerida"
-    } else if (values.clave !== values.repetirClave){
-        errors.repetirClave = "Las contraseñas no concuerdan"
-    }
-
-    return errors
-}
-
-export default UserRegisterForm
-
+export default UserRegisterForm;
