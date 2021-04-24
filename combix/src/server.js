8756c71cc54c7ab5fd18fa3a210202ee095b01b5
marzofran <fpmarzano@gmail.com>
2021-04-23 23:23:05 -0300

    Changed server.js

--- combix/src/server.js
+++ combix/src/server.js
@@ -1,8 +1,6 @@
 const express = require('express'), app = express(), PORT = 3000
 app.use(express.json())
 
-app.listen( PORT, () => console.log(`Server live on port ${PORT}`))
-
 let users = [
     
 ];
@@ -20,3 +18,5 @@ app.post('/users/:id',(request, response) => {
     // return login success code
     // return error code if invalid login data
 })
+
+app.listen( PORT, () => console.log(`Server live on port ${PORT}`))
