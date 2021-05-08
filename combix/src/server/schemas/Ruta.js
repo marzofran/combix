9035301be6716d0ffd8c129f063fe29b3ca284c5
clovergreen24 <75147598+clovergreen24@users.noreply.github.com>
2021-05-08 01:07:40 -0300

    a

--- combix/src/server/schemas/Ruta.js
+++ combix/src/server/schemas/Ruta.js
@@ -2,20 +2,12 @@ const {model, Schema} = require('mongoose');
 const mongoose = require('mongoose');
 
 const rutaSchema = new Schema({
-<<<<<<< HEAD
     origen: {type: mongoose.Schema.Types.ObjectId,ref:'Ciudad'},
     destino: {type: mongoose.Schema.Types.ObjectId,ref:'Ciudad'},
     combi: {type: mongoose.Schema.Types.ObjectId,ref:'Combi'},
     horario: String,
     unavailable: Boolean
 })
-=======
-  origen: {type: mongoose.Schema.Types.Object, ref: 'Ciudad'},
-  destino: {type: mongoose.Schema.Types.Object, ref: 'Ciudad'},
-  combi: {type: mongoose.Schema.Types.Object, ref: 'Combi'},
-  horario: String,
-});
->>>>>>> a9b94e5cb98706de171efc1b684040babe06b47f
 
 const Ruta = model('Ruta', rutaSchema);
 
