92a18415be16324fa5d606339eb9a1b2049ceb46
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-05-27 16:31:36 -0300

    Navegacion para el cliente

---
+++ combix/src/Components/vistas/cliente/misPasajes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MisPasajes = () => {
+  //Esto mapea el elemento pasaje
+  return (
+    <div>
+      <h1>Mis pasajes</h1>
+    </div>
+  );
+};
+
+export default MisPasajes;
